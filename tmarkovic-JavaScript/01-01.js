b15aa114b7684e5b956a18fe25b728bd23e6ab25
Theo Markovic <theodore.markovic@svenskaspel.se>
2017-12-01 13:30:02 +0100

    small fixes

--- tmarkovic-JavaScript/01-01.js
+++ tmarkovic-JavaScript/01-01.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-let calc = (a, b) => { console.log("compairing", a, " + ", b, "=", a === b ? a : 0); return a === b ? Number.parseInt(a) : 0 };
+let calc = (a, b) => { console.log("compairing", a, " + ", b, "=", a === b ? a : 0); return a === b ? ~~a : 0 };
 
 fs.readFile('01-01.txt', function(err, data) {
   if(err) throw err;
@@ -15,18 +15,3 @@ fs.readFile('01-01.txt', function(err, data) {
 
   console.log("Captcha is: ", sum);
 });
-
-
-
-
-
-// let sum = 0;
-// arr.forEach((v, i, a) => {
-//   if(i + 1 != a.length) {
-//     sum += calc(v, a[i + 1]);
-//   } else {
-//     sum += calc(v, a[0])
-//   }
-// });
-
-// console.log(sum);
