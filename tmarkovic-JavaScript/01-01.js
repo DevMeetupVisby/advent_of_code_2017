3bcb51a748b0aceb28fdf468b6cd67f9a0986845
Theo Markovic <theodore.markovic@svenskaspel.se>
2017-12-01 11:23:18 +0100

    Added solution for 01-01

--- tmarkovic-JavaScript/01-01.js
+++ tmarkovic-JavaScript/01-01.js
@@ -1,6 +1,32 @@
 var fs = require('fs');
+let calc = (a, b) => { console.log("compairing", a, " + ", b, "=", a === b ? a : 0); return a === b ? Number.parseInt(a) : 0 };
+
 fs.readFile('01-01.txt', function(err, data) {
-    if(err) throw err;
-    var array = data.toString().split("");
-    console.log(array);
+  if(err) throw err;
+  var array = data.toString().split("");
+  let sum = 0;
+  array.forEach((v, i, a) => {
+    if(i + 1 != a.length) {
+      sum += calc(v, a[i + 1]);
+    } else {
+      sum += calc(v, a[0])
+    }
+  });
+
+  console.log("Captcha is: ", sum);
 });
+
+
+
+
+
+// let sum = 0;
+// arr.forEach((v, i, a) => {
+//   if(i + 1 != a.length) {
+//     sum += calc(v, a[i + 1]);
+//   } else {
+//     sum += calc(v, a[0])
+//   }
+// });
+
+// console.log(sum);
